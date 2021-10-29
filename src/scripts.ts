60a3ab009006cf079cdb760aa5f326bd27ba2bd1
GastonV <atis.sirmais@gmail.com>
2021-10-29 15:12:06 +0300

    add remove task

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,1 +1,32 @@
+/* eslint-disable camelcase */
+const btn__add: HTMLButtonElement = document.querySelector('.btn_add');
+const readTask: HTMLInputElement = document.querySelector('.js__input');
 
+const deleteTask = document.querySelector('.js__delete');
+// eslint-disable-next-line no-unused-vars
+const svgPath = './assets/images/garbage.svg';
+const savedTasks: string[] = [];
+
+btn__add.addEventListener('click', () => {
+  const tag = document.createElement('p');
+  const svg = document.createElement('img');
+  const listStack = document.querySelector('.js__taskList');
+  const taskText = document.createTextNode(readTask.value);
+
+  svg.classList.add('js__delete');
+  svg.setAttribute('src', svgPath);
+  tag.appendChild(svg);
+  tag.setAttribute('width', '100');
+  tag.appendChild(taskText);
+
+  savedTasks.unshift(readTask.value);
+  listStack.insertBefore(tag, listStack.firstElementChild);
+  tag.addEventListener('click', () => {
+    tag.remove();
+  });
+
+  // readTask.value = "";
+});
+
+
+console.log(savedTasks);
